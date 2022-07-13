4e8eeab9e6a56712b5a2985ee83e1e4f83255a51
DVvor <85024600+DVvor@users.noreply.github.com>
2022-07-13 10:17:42 +0200

    install store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import Store from './store'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <Provider store={Store}>
+     <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
