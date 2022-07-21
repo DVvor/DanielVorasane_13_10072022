523a9bd28e3a0f7652281e57dc8997e886e75a97
DVvor <85024600+DVvor@users.noreply.github.com>
2022-07-21 22:25:46 +0200

    navigate page signin when islogged

--- src/Pages/Sign-in/SignIn.jsx
+++ src/Pages/Sign-in/SignIn.jsx
@@ -2,19 +2,22 @@ import './SignIn.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector} from 'react-redux'
 
 import { addItem } from '../../Services/LocalStorage'
 import { getUserInfos, login } from '../../Services/callApi'
 import { logIn } from '../../Store/store'
 
 function SignIn() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   const dispatch = useDispatch()
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [errorConnection, setErrorConnection] = useState(false)
+  const isLoggedIn = useSelector ((state) => state.logState)
+
 
   function RetrieveUsernameInput(event) {
     const inputValue = event.target.value
@@ -41,40 +44,49 @@ function SignIn() {
         addItem('token', token)
         getUserInfos('http://localhost:3001/api/v1/user/profile', {}, token)
         .then(response => {
-          console.log(response)
+          // console.log(response)
           const user = JSON.stringify(response.data.body) // The JSON.stringify() method converts a JavaScript value into a JSON string. 
           addItem('user', user)
         })
         dispatch(logIn()) // Change status is logged in store
         navigate("/user")
+
       }
     })
     .catch( function(error) {
       if(error.response) {
-        console.log("error: login not found")
+        console.log("this error: login not found")
+        setErrorConnection(true)
+        // console.log(errorConnection)
       }
     })
   }
 
+  function checkIsLoggedIn() {
+    if(isLoggedIn) {
+      navigate("/user")
+    }
+  }
+
+  useEffect(() => {
+    checkIsLoggedIn()
+  })
+
   return (
     <main className="main bg-dark">
       <div className='sign-in-content'>
-        {/* <i className="fa fa-user-circle sign-in-icon"></i> */}
         <FontAwesomeIcon icon={faCircleUser} className="sign-in-icon" />
-
         <h1>Sign In</h1>
-
         <form onSubmit={handleSubmit}>
           <div className="input-wrapper">
             <label name="username"> Username </label>
             <input type="text" id="username" onChange={RetrieveUsernameInput}  /> 
-            {/* style error css */}
-            {/*  className={isLoggedIn ? "" : "error"} */}
           </div>
           <div className="input-wrapper">
             <label name="password"> Password </label>
             <input type="password" id="password" onChange={RetrievePasswordInput} />
           </div>
+          <p className='errormessage'>{errorConnection? "L'email ou/et le mot de passe est incorrecte. Veuillez recommencer": ""}</p>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
             <label name="remember-me">Remember me </label>
